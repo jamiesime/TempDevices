385d2d97fc95de5a7fd40596acf8e0c274edce45
GHOST-HUB\Jamie <jamiesime@gmail.com>
2020-01-25 15:52:31 +0000

    initial commit

---
+++ src/main/java/com/example/TemperatureDevices/TemperatureDevicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.TemperatureDevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureDevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureDevicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TemperatureDevices/TemperatureDevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TemperatureDevices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureDevicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
